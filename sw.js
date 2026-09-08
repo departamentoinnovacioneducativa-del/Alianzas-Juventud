27cb64fbcbbc1d7f11fd4f840f7769e577cf160e
DIE <departamentoinnovacioneducativa@juventud.edu.mx>
2026-09-08 12:22:41 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'alianzas-v1.1.3.2'; // Cambia esta versión para forzar actualización
+const CACHE_NAME = 'alianzas-v1.1.3.3'; // Cambia esta versión para forzar actualización
 const urlsToCache = [
   './',
   './index.html',
